c63368f158dd7c7c53cc1e17cce1f133103a50fb
gabrielacsalesc <75990722+gabrielacsalesc@users.noreply.github.com>
2021-06-13 17:21:30 -0300

    Add files via upload

---
+++ src/Header.js
@@ -0,0 +1,8 @@
+function Header (props){
+    return(
+        <nav>
+            <h1>{props.title}</h1>
+        </nav>
+    )
+}
+export default Header
---
+++ src/service/dados.js
@@ -0,0 +1,29 @@
+
+const musicas = [
+    {
+        id: 1,
+        nome: "King Princess",
+        genero: "pop",
+        capa:"https://lh3.googleusercontent.com/proxy/XKL4w4b_ZhUr4WtDAyFAtY-dvO6NES90X3JR1GxY7xV0UPY50TjavOcjr0OEjUDkQkAVG89_Pi6EGPm0OANfN58NjbHG4nVa5u_AlNiV05ogZPpkkVokKDHq4nhx"
+    },
+    {
+        id: 2,
+        nome: "Lady Gaga",
+        genero: "pop",
+        capa:"https://veja.abril.com.br/wp-content/uploads/2020/12/LADY-GAGA-INSTAGRAM-@LADYGAGA-25-2020.jpg.jpg?quality=70&strip=info"
+    },
+    {
+        id: 3,
+        nome: "The Weeknd",
+        genero: "R&B",
+        capa:"https://portalpopline.com.br/wp-content/uploads/2021/03/Feature-The-Weeknd-1-Billboard-Micaiah-Carter-1240-1611679620-compressed-1.jpg"
+    },
+    {
+        id: 4,
+        nome: "Billie Eilish",
+        genero: "Alternativo",
+        capa:"https://yt3.ggpht.com/ytc/AAUvwnjyKzvLyxW8YJV6nSRC71JFUikN6ICJn_v-53mz1Q=s900-c-k-c0x00ffffff-no-rj"
+    }
+]
+
+export default musicas
